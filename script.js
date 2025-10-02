1d1bc1b5a720124f25997a1c15009a73bcda5a2f
Ordep <pedro.mantovanini.silva@escola.pr.gov.br>
2025-10-02 08:56:59 -0300

    Add files via upload

--- script.js
+++
@@ -1,4 +0,0 @@
-var mapaimages = document.querySelectorAll("#quad img");
-for(i=0; i<mapaimages.length; i++) {
-  mapaimages[i].addEventListener('click', function(){ this.classList.toggle("expanded")}); 
-}
